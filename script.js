141216e96b8eb9e176df363900522f0fe1a92749
Alwin Sajan <116751380+Alwin-Sajan@users.noreply.github.com>
2023-08-22 12:19:41 +0530

    Update 2

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 
 let leaf = document.getElementById('ASP');
 let sidet = document.getElementById('sideer');
+let text = document.getElementById('Text');
+// let buble = document.getElementById('buble');
 
 
 
+let bio = document.getElementById('bio');
+var wid = window.innerWidth;
 
+if (wid > 450) {
+    window.addEventListener('scroll', () => {
+        let value = window.scrollY;
+        let endf = window.scrollY.toFixed();
+        bio.style.left = value * 1.5 + 'px';
+        text.style.left = value * -1.5 + 'px';
+        text.style.top = value * -0.2 + 'px';
+        // buble.style.left = value * 1 + 'px';
+
+    });
+}
+// else{
+//     window.addEventListener('scroll', () => {
+//         let value = window.scrollY;
+//         let endf = window.scrollY.toFixed();
+       
+
+
+//     });
+// }
+
+    
 
     window.addEventListener('scroll', () =>{
         let value = window.scrollY;
         let endf = window.scrollY.toFixed;
+        
+
 
         leaf.style.top = value * -1.5 + 'px';
+        
 
+        
 
         sidet.style.right = value * -0.75 + 'px';
         sidet.style.top = value * -0.75 + 'px';
