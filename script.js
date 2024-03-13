d8329e881b021c0194101a00da95760e750f0f39
Alwin Sajan <116751380+Alwin-Sajan@users.noreply.github.com>
2024-03-13 22:26:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
-
 let leaf = document.getElementById('ASP');
 let sidet = document.getElementById('sideer');
 let text = document.getElementById('Text');
-// let buble = document.getElementById('buble');
-
-
-
 let tag = document.getElementById('tag');
 var wid = window.innerWidth;
+
+let bgchanger = document.getElementById('home'); // Change this to the element you want to change the background color
 
 if (wid > 450) {
     window.addEventListener('scroll', () => {
         let value = window.scrollY;
-        let endf = window.scrollY.toFixed();
         tag.style.left = value * 1.5 + 'px';
         text.style.left = value * -1.5 + 'px';
         text.style.top = value * -0.2 + 'px';
-        // buble.style.left = value * 1 + 'px';
 
+        // Check the scroll position dynamically
+        if (value < 250) {
+            bgchanger.style.transition = "background-color 0.5s ease"; // Set the transition properties
+            bgchanger.style.backgroundColor = "";
+        } else {
+            // Reset the background color if the scroll position is greater than 100
+            bgchanger.style.transition = "background-color 0.5s ease"; // Set the transition properties
+            bgchanger.style.backgroundColor = "black";
+        }
     });
 }
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+    leaf.style.top = value * -1.5 + 'px';
+    sidet.style.right = value * -0.75 + 'px';
+    sidet.style.top = value * -0.75 + 'px';
+});
+
+
+
+
+
+
 // else{
 //     window.addEventListener('scroll', () => {
 //         let value = window.scrollY;
@@ -30,23 +47,3 @@ if (wid > 450) {
 //     });
 // }
 
-
-
-    
-
-    window.addEventListener('scroll', () =>{
-        let value = window.scrollY;
-        let endf = window.scrollY.toFixed;
-        
-
-
-        leaf.style.top = value * -1.5 + 'px';
-        
-
-        
-
-        sidet.style.right = value * -0.75 + 'px';
-        sidet.style.top = value * -0.75 + 'px';
-  
-
-})
