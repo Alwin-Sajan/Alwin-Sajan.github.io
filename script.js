ade5ebd36fce911bbcd4473f97ccd53eba988256
Alwin Sajan <116751380+Alwin-Sajan@users.noreply.github.com>
2024-03-16 11:49:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,34 @@ let leaf = document.getElementById('ASP');
 let sidet = document.getElementById('sideer');
 let text = document.getElementById('Text');
 let tag = document.getElementById('tag');
-var wid = window.innerWidth;
+let bgchanger = document.getElementById('home');
+let wid = window.innerWidth;
 
-let bgchanger = document.getElementById('home'); // Change this to the element you want to change the background color
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElementsL = document.querySelectorAll('.hiddenL'); // Assuming 'bio' is a class
+hiddenElementsL.forEach((el) => observer.observe(el));
+
+const hiddenElementsR = document.querySelectorAll('.hiddenR'); // Assuming 'bio' is a class
+hiddenElementsR.forEach((el) => observer.observe(el));
+
+const hiddenElementsB = document.querySelectorAll('.hiddenB'); // Assuming 'bio' is a class
+hiddenElementsB.forEach((el) => observer.observe(el));
+
+const hiddenElementsD = document.querySelectorAll('.hiddenD'); // Assuming 'bio' is a class
+hiddenElementsD.forEach((el) => observer.observe(el));
+
+const hiddenElementsLm = document.querySelectorAll('.hiddenLm'); // Assuming 'bio' is a class
+hiddenElementsLm.forEach((el) => observer.observe(el));
 
 if (wid > 450) {
     window.addEventListener('scroll', () => {
@@ -13,13 +38,15 @@ if (wid > 450) {
         text.style.left = value * -1.5 + 'px';
         text.style.top = value * -0.2 + 'px';
 
-        // Check the scroll position dynamically
+        // Animation
+        
+
+        // Check the scroll position dynamically for background color change
         if (value < 250) {
-            bgchanger.style.transition = "background-color 0.5s ease"; // Set the transition properties
+            bgchanger.style.transition = "background-color 0.5s ease";
             bgchanger.style.backgroundColor = "";
         } else {
-            // Reset the background color if the scroll position is greater than 100
-            bgchanger.style.transition = "background-color 0.5s ease"; // Set the transition properties
+            bgchanger.style.transition = "background-color 0.5s ease";
             bgchanger.style.backgroundColor = "black";
         }
     });
@@ -31,19 +58,3 @@ window.addEventListener('scroll', () => {
     sidet.style.right = value * -0.75 + 'px';
     sidet.style.top = value * -0.75 + 'px';
 });
-
-
-
-
-
-
-// else{
-//     window.addEventListener('scroll', () => {
-//         let value = window.scrollY;
-//         let endf = window.scrollY.toFixed();
-       
-
-
-//     });
-// }
-
